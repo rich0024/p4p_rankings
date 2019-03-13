d17f7812cb65e7959a397a4dffa0ed91e0e81a04
Richard Yance <yance0022@gmail.com>
2019-03-13 19:45:25 +0000

    initial commit

---
+++ lib/p4p_rankings.rb
@@ -0,0 +1,6 @@
+require "p4p_rankings/version"
+
+module P4pRankings
+  class Error < StandardError; end
+  # Your code goes here...
+end
---
+++ lib/p4p_rankings/version.rb
@@ -0,0 +1,3 @@
+module P4pRankings
+  VERSION = "0.1.0"
+end
