100704ec34bdf27c081a3890c0760cce306c8eea
Richard Yance <yance0022@gmail.com>
2019-03-18 02:59:59 +0000

    CLI edit 2

--- lib/p4p_rankings/cli.rb
+++ lib/p4p_rankings/cli.rb
@@ -3,6 +3,7 @@ class P4pRankings::CLI
   def call
     puts "running scraper"
     P4pRankings::Fighters.scrape_fighters
+    P4pRankings::Fighters.rankings
   end
 
 
--- lib/p4p_rankings/fighters.rb
+++ lib/p4p_rankings/fighters.rb
@@ -1,7 +1,12 @@
 class P4pRankings::Fighters
 
   def self.scrape_fighters
-    binding.pry
+    doc = Nokogiri::HTML(open("http://boxrec.com/en/ratings?nbf%5Bcountry%5D=&nbf%5Bdivision%5D=&nbf%5Bsex%5D=M&nbf%5Bstance%5D=&nbf%5Bstatus%5D=a&r_go="))
+    fighter = doc.css("a.personLink").text
+  end
+
+  def rankings
+    fighter.each_with_index{ |value, index| puts "#{index}. #{value}"}
   end
 
 end
