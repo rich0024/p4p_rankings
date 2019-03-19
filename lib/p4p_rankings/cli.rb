7435d4b7569933438a083c944327bd19fdf13ad2
Richard Yance <yance0022@gmail.com>
2019-03-19 02:55:00 +0000

    names and record

--- lib/p4p_rankings/cli.rb
+++ lib/p4p_rankings/cli.rb
@@ -2,8 +2,7 @@ class P4pRankings::CLI
 
   def call
     puts "running scraper"
-    P4pRankings::Fighters.scrape_fighters
-    P4pRankings::Fighters.rankings
+    P4pRankings::Fighters.all
   end
 
 
--- lib/p4p_rankings/fighters.rb
+++ lib/p4p_rankings/fighters.rb
@@ -1,12 +1,28 @@
 class P4pRankings::Fighters
+  attr_accessor :name, :record, :win, :loss, :draw
+  @@all = []
+
+  def initialize
+    name = @name
+    record = @record
+    win = @win
+    loss = @loss
+    draw = @draw
+  end
+
+  def self.all
+    binding.pry
+  end
 
   def self.scrape_fighters
     doc = Nokogiri::HTML(open("http://boxrec.com/en/ratings?nbf%5Bcountry%5D=&nbf%5Bdivision%5D=&nbf%5Bsex%5D=M&nbf%5Bstance%5D=&nbf%5Bstatus%5D=a&r_go="))
-    fighter = doc.css("a.personLink").text
+    doc.css('tr').collect do |fighter|
+      {
+      name: fighter.css("a.personLink").text,
+      record: "#{win = fighter.css('span.textWon').text} - #{loss = fighter.css('span.textLost').text} - #{draw = fighter.css('span.textDraw').text}"
+      }
+    end
   end
 
-  def rankings
-    fighter.each_with_index{ |value, index| puts "#{index}. #{value}"}
-  end
 
 end
